8cc49431b8caa6bdcf6f28f923b4a7783914b66c
Oswaldo Morales <morales.oswaldo151@gmail.com>
2026-05-02 22:02:27 -0600

    Se agregan las funciones para verificar booleanos, inferir valores y perfilar columnas

--- main.py
+++ main.py
@@ -31,4 +31,78 @@ def es_fecha(valor):
             pass
     return False
 
+def es_booleano(valor):
+    v = str(valor).strip().lower()
+    return v in ['true','false','yes','no','si','1','0','t','f']
+
+
+
+#Infiere el tipo de dato de una columna 
+def inferir_tipo(valores):
+
+    valores_validos = []
+
+    for v in valores:
+        if not es_valor_nulo(v):
+            valores_validos.append(v)
+
+    if not valores_validos:
+        return "texto"
     
+    total = len(valores_validos)
+    umbral = 0.8
+
+    num_fechas = sum(1 for v in valores_validos if es_fecha(v))
+    num_booleanos = sum(1 for v in valores_validos if es_booleano(v))
+    num_numericos = sum(1 for v in valores_validos if es_valor_numerico(v))
+    
+    if num_fechas / total >= umbral:
+        return "fecha"
+    elif num_booleanos / total >= umbral:
+        return "booleano"
+    elif num_numericos / total >= umbral:
+        return "numerico"
+    else:
+        return "texto"
+    
+
+def perfilar_columna(nombre,valores):
+    
+    total = len(valores)
+    nulos = 0
+
+    for valor in valores:
+        if es_valor_nulo(valores):
+            nulos+=1
+
+    valores_no_nulos = []
+
+    for valor in valores:
+        if not es_valor_nulo:
+            valores_no_nulos.append(valor)
+    
+    unicos = len(set(valores_no_nulos))
+
+    if len(valores_no_nulos>0):
+        ejemplo = valores_no_nulos[0]
+    else:
+        ejemplo= ""
+
+    tipo = inferir_tipo(valores)
+
+    porc_nulos = round(nulos / total * 100, 2) if total > 0 else 0.00
+    porc_unicos = round(unicos / total * 100, 2) if total > 0 else 0.00
+
+    return {
+        "nombre_columna": nombre,
+        "tipo_inferido": tipo,
+        "total_registros": total,
+        "valores_nulos": nulos,
+        "porcentaje_nulos": porc_nulos,
+        "valores_unicos": unicos,
+        "porcentaje_unicos": porc_unicos,
+        "ejemplo_valor": ejemplo
+    }
+
+
+
